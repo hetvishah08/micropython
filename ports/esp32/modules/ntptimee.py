aa66514f5fca3428088906bc4e6b7797c0421dc2
Hetvi Shah <hetvi.shah88@gmail.com>
2019-11-27 15:43:41 +0530

    ntptime providing IST value

---
+++ ports/esp32/modules/ntptimee.py
@@ -0,0 +1,77 @@
+
+try:
+    import usocket as socket
+except:
+    import socket
+try:
+    import ustruct as struct
+except:
+    import struct
+
+# (date(2000, 1, 1) - date(1900, 1, 1)).days * 24*60*60
+NTP_DELTA = 3155673600
+
+#NTP server was enabled on a router at 192.168.11.1.
+#Public server could be used such as pool.ntp.org.
+host = "pool.ntp.org"
+
+def time():
+    NTP_QUERY = bytearray(48)
+    NTP_QUERY[0] = 0x1b
+    addr = socket.getaddrinfo(host, 123)[0][-1]
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(1)
+    res = s.sendto(NTP_QUERY, addr)
+    msg = s.recv(48)
+    s.close()
+    val = struct.unpack("!I", msg[40:44])[0]
+    t = val - NTP_DELTA + 19800 # GMT to IST conversion add 19800
+    return t
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
